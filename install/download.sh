c2358c4722f474b8a8ab6cc0a769769d18a63730
Pavan Challa <pavanchalla@wusPC186029-124.local>
2015-10-26 14:23:32 +0000

    fixes

--- install/download.sh
+++ install/download.sh
@@ -44,20 +44,20 @@ echo "7. download maven, if not already there"
 echo "8. download java 8, if not already there"
 echo ""
 
-if [ ! -e "~/Downloads" ]; then
-    mkdir "~/Downloads"
+if [ ! -e "/root/Downloads" ]; then
+    mkdir "/root/Downloads"
 fi
 
 
-if [ ! -e "~/Downloads/apache-maven-$MAVEN_VERSION-bin.tar.gz" ]; then
+if [ ! -e "/root/Downloads/apache-maven-$MAVEN_VERSION-bin.tar.gz" ]; then
     echo "Downloading Maven"
-    curl -Lo "~/Downloads/apache-maven-$MAVEN_VERSION-bin.tar.gz" http://mirror.sdunix.com/apache/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
+    curl -Lo "/root/Downloads/apache-maven-$MAVEN_VERSION-bin.tar.gz" http://mirror.sdunix.com/apache/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz
 else
     echo "Skipping Maven download because it already exists"
 fi
 echo "7. Download Maven: OK"
 
-if [ ! -e "~/Downloads/$JDK_PACKAGE.tar.gz" ]; then
+if [ ! -e "/root/Downloads/$JDK_PACKAGE.tar.gz" ]; then
     echo "Downloading Java 8"
     curl -j -k -L -H "Cookie: oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/$JDK_PACKAGE.tar.gz" > /root/Downloads/$JDK_PACKAGE.tar.gz
 else
